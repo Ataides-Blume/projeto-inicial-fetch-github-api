2bbe250511b38e698b83d33fe18fca6074a22f8f
Ataides-Blume <abo.revelacao@outlook.com>
2025-06-26 22:28:10 -0300

    alterado user fuction vazia

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,21 @@
-async function user(userName){
+async function user(){
     const response = await fetch('https://api.github.com/users/ataides-blume')
     return await response.json()
 }
 
 console.log(await user())
 
-function getUserProfile(userName){
-    user(userName).then(userData => {
-        let userInfo = `<img src="${userData.avatar_url}" alt="Foto de perfil do usuário" />
-        <div class="data">
-        <h1>${userData.name ?? 'Não possui nome cadastrado'}</h1>
-        <p>${userData.bio ?? 'Não possui bio cadastrada'}</p> </div>`
+// function getUserProfile(userName){
+//     user(userName).then(userData => {
+//         let userInfo = `<img src="${userData.avatar_url}" alt="Foto de perfil do usuário" />
+//         <div class="data">
+//         <h1>${userData.name ?? 'Não possui nome cadastrado'}</h1>
+//         <p>${userData.bio ?? 'Não possui bio cadastrada'}</p> </div>`
 
-        document.querySelector('.profile-data').innerHTML = userInfo
+//         document.querySelector('.profile-data').innerHTML = userInfo
 
         
-    })
-}
+//     })
+// }
 
-getUserProfile('ataides-blume')
+// getUserProfile('ataides-blume')
