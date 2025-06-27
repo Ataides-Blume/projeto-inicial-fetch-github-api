45a85047963a6c01e5f3e4aa79b2eaeb06682936
Ataides-Blume <abo.revelacao@outlook.com>
2025-06-27 16:32:07 -0300

    alterando nome   por userName no na function js.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,21 @@
-async function user(){
-    const response = await fetch('https://api.github.com/users/ataides-blume')
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value 
+    getUserProfile(userName)
+})
+
+async function user(userName){
+    const response = await fetch(`https://api.github.com/users/${userName}`)
     return await response.json()
 }
 
-console.log(await user())
+function getUserProfile(userName){
+    user(userName).then(userData => {
+        let userInfo = `<img src="${userData.avatar_url}" alt="Foto de perfil do usuário" />
+                    <div class="data">
+                        <h1>${userData.name ?? 'Não possui nome cadastrado'}</h1>
+                        <p>${userData.bio ?? 'Não possui bio cadastrada'}</p>
+                    </div>`
 
-// function getUserProfile(userName){
-//     user(userName).then(userData => {
-//         let userInfo = `<img src="${userData.avatar_url}" alt="Foto de perfil do usuário" />
-//         <div class="data">
-//         <h1>${userData.name ?? 'Não possui nome cadastrado'}</h1>
-//         <p>${userData.bio ?? 'Não possui bio cadastrada'}</p> </div>`
-
-//         document.querySelector('.profile-data').innerHTML = userInfo
-
-        
-//     })
-// }
-
-// getUserProfile('ataides-blume')
+        document.querySelector('.profile-data').innerHTML = userInfo
+    })
+}
